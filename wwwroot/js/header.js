a8e9f0bb17431eb705bae27a7a157dacd6b582e0
satadda <sadda@usf.edu>
2024-04-22 14:16:36 -0400

    Added code for New Data Form and Inquiry Form views, Added css styling for all views

---
+++ wwwroot/js/header.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var a = document.getElementById("myCenter");
+    if (a.className === "center") {
+        a.className += " responsive";
+    } else {
+        a.className = "center";
+    }
+}
